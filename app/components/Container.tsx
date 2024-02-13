1d236bf304577191e693076999465e835e75ebcf
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-13 22:48:28 +0100

    Adding a container

---
+++ app/components/Container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps{
+    children: React.ReactNode;
+    className?: string;
+}
+
+
+const Container: React.FC<ContainerProps> = ( {children, className = ''}) => {
+    return ( 
+    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
+        {children}
+    </div> 
+    );
+}
+ 
+export default Container;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Container from "./components/Container";
+
 export default function Home() {
   return (
-   <div>Start</div>
+   <main className="flex items-center text-dark w-full min-h-screen">
+    <Container>
+      <div>Start</div>
+    </Container>
+   </main>
+  
   );
 }
