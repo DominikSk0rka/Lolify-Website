bf9e0db66f115dc2241fbaa15c16dd5c981abfe1
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-15 23:31:46 +0100

    responsive nulldata

--- app/components/NullData.tsx
+++ app/components/NullData.tsx
@@ -1,50 +1,83 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { TypeAnimation } from "react-type-animation";
 import Container from "./inputs/Container";
 import AnimatedText from "./animations/AnimatedText";
-import { TypeAnimation } from "react-type-animation";
 
 const NullData = () => {
   return (
-    <main className="flex text-dark w-full min-h-screen dark:text-light">
-      <Container className="p-0">
-        <AnimatedText text="Oops..." className=" text-center" />
+    <div>
+      <div className="flex text-dark w-full min-h-screen dark:text-light xl:hidden">
+        <Container className="p-0">
+          <AnimatedText text="Oops..." className=" text-center" />
 
-        <div className="flex items-center justify-between w-full ">
-          <Image
-            src="/animations/bard.gif"
-            alt="animacja"
-            width={600}
-            height={600}
-            className="w-auto h-auto"
-          />
+          <div className="flex items-center justify-between w-full ">
+            <Image
+              src="/animations/bard.gif"
+              alt="animacja"
+              width={600}
+              height={600}
+              className="w-auto h-auto"
+            />
 
-          <div className="w-1/2 flex flex-col items-center">
-            <p className="mt-2 font-medium self-start text-start">
-              <TypeAnimation
-                sequence={[
-                  "Bard guards this page. You have no access. Try again, summoner!",
-                  3000,
-                  "Bard guards this page. You have no access. If you want to challenge me come back stronger with admin account :P",
-                ]}
-                wrapper="span"
-                speed={40}
-                style={{ fontSize: "2em", display: "inline-block" }}
-              />
-            </p>
-            <div className="flex mt-8 gap-2 xl:self-start">
-              <Link
-                href="/"
-                className="bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
-              >
-                Go back
-              </Link>
+            <div className="w-1/2 flex flex-col items-center">
+              <p className="mt-2 font-medium self-start text-start">
+                <TypeAnimation
+                  sequence={[
+                    "Bard guards this page. You have no access. Try again, summoner!",
+                    3000,
+                    "Bard guards this page. You have no access. If you want to challenge me come back stronger with admin account :P",
+                  ]}
+                  wrapper="span"
+                  speed={40}
+                  style={{ fontSize: "2em", display: "inline-block" }}
+                />
+              </p>
+              <div className="flex mt-8 gap-2 self-start">
+                <Link
+                  href="/"
+                  className="bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+                >
+                  Go back
+                </Link>
+              </div>
             </div>
           </div>
+        </Container>
+      </div>
+
+      <div className="flex flex-col text-dark w-gull  dark:text-light">
+        <AnimatedText text="Oops..." className="!text-2xl" />
+        <p className="mt-2 font-medium self-start text-start xl:!text-center">
+          <TypeAnimation
+            sequence={[
+              "Bard guards this page. You have no access. Try again, summoner!",
+              3000,
+              "Bard guards this page. You have no access. If you want to challenge me come back stronger with admin account :P",
+            ]}
+            wrapper="span"
+            speed={40}
+            style={{ fontSize: "2em", display: "inline-block" }}
+          />
+        </p>
+        <div className="flex mt-8 gap-2 self-center">
+          <Link
+            href="/"
+            className="bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+          >
+            Go back
+          </Link>
         </div>
-      </Container>
-    </main>
+        <Image
+          src="/animations/bard.gif"
+          alt="animacja"
+          width={600}
+          height={600}
+          className="w-30 h-30"
+        />
+      </div>
+    </div>
   );
 };
 
