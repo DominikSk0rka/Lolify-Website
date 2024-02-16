1041c36154d64ce893c8e51faea6bf706d406e26
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-16 21:41:16 +0100

    added alphabetical sorting algorithm

--- app/champions/page.tsx
+++ app/champions/page.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import ChampionCard from "../components/championsDisplay/ChampionCard";
 import Container from "../components/inputs/Container";
-import Heading from "../components/inputs/Heading";
 import AnimatedText from "../components/animations/AnimatedText";
 
 const Champions = () => {
@@ -28,13 +27,16 @@ const Champions = () => {
     console.log(champions);
   }, [champions]);
 
+  const shuffledchampions = champions
+    .slice()
+    .sort((a, b) => a.name.localeCompare(b.name));
   return (
     <main className="dark:bg-dark ">
       <AnimatedText text="Champions" className="text-center" />
       <Container className="p-16 xl:p-16">
         <div className="grid grid-cols-5 sm:grid-cols-2 lg:grid-cols-2 xl-grid-cols-3 2xl:grid-cols-3 gap-8">
-          {champions.map((champion) => (
-            <ChampionCard key={champion} champion={champion} />
+          {shuffledchampions.map((champion) => (
+            <ChampionCard key={champion.id} champion={champion} />
           ))}
         </div>
       </Container>
