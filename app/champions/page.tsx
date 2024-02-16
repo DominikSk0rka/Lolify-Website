f315f2e4d0fd5fec683623635a2372cdb73fc87f
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-16 19:50:54 +0100

    displaying champions from api

--- app/champions/page.tsx
+++ app/champions/page.tsx
@@ -1,17 +1,38 @@
-import AnimatedText from "../components/animations/AnimatedText";
-import Container from "../components/inputs/Container";
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
-const champions = async () => {
+const Champions = () => {
+  const [champions, setChampions] = useState([]);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    axios
+      .get("https://lolify.fly.dev/api/champion", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        setChampions(response.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(champions);
+  }, [champions]);
+
   return (
-    <Container>
-      <main>
-        <AnimatedText
-          text="Choose your champion"
-          className="pb-3 text-center"
-        />
-      </main>
-    </Container>
+    <div>
+      {champions.map((champion) => (
+        <div key={champion.id}>
+          <h2>{champion.name}</h2>
+          <img src={champion.image_link} alt={champion.name} />
+        </div>
+      ))}
+    </div>
   );
 };
 
-export default champions;
+export default Champions;
--- app/components/championsDisplay/ChampionCard.tsx
+++ app/components/championsDisplay/ChampionCard.tsx
@@ -1,31 +1,15 @@
 "use client";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 interface ChampionCardProps {
-  data: any;
+  champion: any;
 }
 
-const ChampionCard: React.FC<ChampionCardProps> = ({ data }) => {
-  const router = useRouter();
-
+const ChampionCard: React.FC<ChampionCardProps> = ({ champion }) => {
   return (
-    <div
-      onClick={() => router.push(`/product/${data.id}`)}
-      className="col-span-1 cursor-pointer  rounded-sm p-2 transition hover:scale-105 text-center text-sm"
-    >
+    <div className="col-span-1 cursor-pointer  rounded-sm p-2 transition hover:scale-105 text-center text-sm">
       <div className="flex flex-col items-center w-full gap-1">
-        <div className="aspect-square overflow-hidden relative w-full">
-          <Image
-            fill
-            src={data.images[0].image}
-            alt={data.name}
-            className="w-full h-full object-contain"
-          />
-        </div>
-
+        <div className="aspect-square overflow-hidden relative w-full"></div>
         <div className="mt-4">(data.name)</div>
-        <div className="font-semibold">(data.price)</div>
       </div>
     </div>
   );
