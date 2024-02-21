716d0f49899fd1448ae40119c03f2f6c313bc0d3
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-21 21:49:20 +0100

    adding, searching by roles

--- app/champions/page.tsx
+++ app/champions/page.tsx
@@ -12,11 +12,18 @@ interface Champion {
 
 const Champions = () => {
   const [champions, setChampions] = useState<Champion[]>([]);
+  const [selectedRole, setSelectedRole] = useState<number | null>(null);
 
   useEffect(() => {
     const token = Cookies.get("token");
+    let apiUrl = "https://lolify.fly.dev/api/champion/";
+
+    if (selectedRole !== null) {
+      apiUrl = `https://lolify.fly.dev/api/champion/role/${selectedRole}`;
+    }
+
     axios
-      .get("https://lolify.fly.dev/api/champion/", {
+      .get(apiUrl, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -24,7 +31,7 @@ const Champions = () => {
       .then((response) => {
         setChampions(response.data);
       });
-  }, []);
+  }, [selectedRole]);
 
   useEffect(() => {
     console.log(champions);
@@ -33,14 +40,50 @@ const Champions = () => {
   const shuffledchampions = champions
     .slice()
     .sort((a, b) => a.name.localeCompare(b.name));
+
+  const handleRoleButtonClick = (role: number | null) => {
+    setSelectedRole(role);
+  };
+
   return (
     <main className="dark:bg-dark p-8 w-full h-full">
       <div className="dark:text-light flex flex-row justify-center items-center gap-2">
-        <button>ALL</button>
-        <button>JUNGLE</button>
-        <button>MID</button>
-        <button>ADC</button>
-        <button>SUPPORT</button>
+        <button
+          onClick={() => handleRoleButtonClick(null)}
+          className="flex items-center bg-dark text-light p-2.5 px-4 rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+        >
+          ALL
+        </button>
+        <button
+          onClick={() => handleRoleButtonClick(1)}
+          className="flex items-center bg-dark text-light p-2.5 px-4 md:px-2  rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+        >
+          TOP
+        </button>
+        <button
+          onClick={() => handleRoleButtonClick(2)}
+          className="flex items-center bg-dark text-light p-2.5 px-4 md:px-2  rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+        >
+          JUNGLE
+        </button>
+        <button
+          onClick={() => handleRoleButtonClick(3)}
+          className="flex items-center bg-dark text-light p-2.5 px-4 md:px-2  rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+        >
+          MID
+        </button>
+        <button
+          onClick={() => handleRoleButtonClick(4)}
+          className="flex items-center bg-dark text-light p-2.5 px-4 md:px-2  rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+        >
+          ADC
+        </button>
+        <button
+          onClick={() => handleRoleButtonClick(5)}
+          className="flex items-center bg-dark text-light p-2.5 px-4 md:px-2 rounded-lg text-lg font-semibold transition hover:scale-105 dark:bg-light dark:text-dark"
+        >
+          SUPPORT
+        </button>
       </div>
       <div className="text-center text-3xl font-bold pt-10 dark:text-light pb-10">
         Champions{" "}
