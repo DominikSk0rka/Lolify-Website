4d3b3a8a3322cfc8e21332bc858a7ea07bcdb48c
Dominik Skórka <Sk0rka.dev@gmail.com>
2024-02-23 15:02:17 +0100

    cypress register form test

---
+++ cypress/e2e/register.cy.ts
@@ -0,0 +1,34 @@
+describe("Registration Form", () => {
+  it("successfully registers a new user", () => {
+    cy.visit("https://lolify.vercel.app/register");
+
+    cy.get("#name").type("invalid_name");
+    cy.get("#email").type("invalid_email");
+    cy.get("#password").type("password12");
+    cy.get("#password_confirmation").type("password123");
+
+    cy.get("button").contains("Register").click();
+
+    cy.contains("Registration successful").should("exist");
+
+    cy.url().should("eq", "https://lolify.vercel.app/");
+
+    cy.getCookie("token").should("exist");
+  });
+
+  it("handles registration failure with invalid data", () => {
+    cy.visit("https://lolify.vercel.app/register");
+
+    cy.get("#name").type("John Doe");
+    cy.get("#email").type("tostemail@gmail.com");
+    cy.get("#password").type("password123");
+    cy.get("#password_confirmation").type("password123");
+
+    cy.get("button").contains("Register").click();
+
+    cy.contains("Registration failed: Invalid email address").should("exist");
+
+    cy.url().should("not.eq", "https://lolify.vercel.app/");
+    cy.getCookie("token").should("not.exist");
+  });
+});
